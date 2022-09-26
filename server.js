67726c05daeb3ea6333ab0485f41a332af4ce5d3
Daniela Lopez <dlope0831@gmail.com>
2022-09-26 18:12:16 -0500

    revised server js file

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
-const PORT = 3001;
-
 const app = express();
+const PORT = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
 app.use(express.static('public'));
 
+require('./routes/apiRoutes' (app));
+
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+    console.log(`App listening at http://localhost:${PORT}`);
   });
