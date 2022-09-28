e6c23052e5aa81f2dc73b98ac1203e1b8eb8cea3
Daniela Lopez <dlope0831@gmail.com>
2022-09-28 18:29:33 -0500

    verifying that port is working

--- server.js
+++ server.js
@@ -1,32 +1,20 @@
 const express = require('express');
-const { notes } = require('./Develop/data/db.json');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
-// const htmlRoutes = require('./routes/htmlRoutes');
-// const apiRoutes = require('./routes/apiRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('./develop/public'));
 
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
 
-app.get('/api/notes', (req, res) => {
-    let result = notes;
-        res.json(result);
-    
-  });
+// app.get("/api/notes", function(req, res) {
 
-app.post('/api/notes', (req, res) =>{
-    req.body = notes.length.toString();
-  // if any data in req.body is incorrect, send 400 error back
-
-    const notes = createNewNote(req.body, notes);
-    res.json(notes);
-});
-
+// })
 
 app.listen(PORT, () => {
     console.log(`Note server now on port ${PORT}!`);
