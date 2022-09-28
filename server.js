f935e961f5af0f73f51ae434ac9c08b6ef1c5aa7
Daniela Lopez <dlope0831@gmail.com>
2022-09-28 18:34:55 -0500

    creating not get request

--- server.js
+++ server.js
@@ -2,19 +2,26 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
+const { json } = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('./develop/public'));
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// app.get("/api/notes", function(req, res) {
 
-// })
+app.get("/api/notes", function(req, res) {
+readFileAsync("./develop/db/db.json", "utf-8")
+.then(function(data) {
+    notes= [].concat(JSON.parse(data))
+    res.json(notes)
+    })
+});
 
 app.listen(PORT, () => {
     console.log(`Note server now on port ${PORT}!`);
