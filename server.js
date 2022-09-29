d66ec8010f8836084988042b7664984f6a76b114
Daniela Lopez <dlope0831@gmail.com>
2022-09-28 22:46:09 -0500

    add heroku

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-const { json } = require('express');
+const getRoutes = require('./Develop/Routes/routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static('./develop/public'));
+app.use(express.static('./Develop/public'));
+app.use('/', getRoutes);
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -27,16 +28,20 @@ app.post("/api/notes", function(req,res) {
     const note = req.body;
     readFileAsync("./develop/db/db.json", "utf-8")
     .then(function(data) {
-        const notes = [].concat(JSON.parse(data));
-        for (let i = 0; i < notes.length; i++);
+        const notes = JSON.parse(data);
+
         notes.push(note);
+        console.log(notes)
         return notes 
     })
     .then (function(notes) {
         writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
-        res.json(notes);
+        res.json(notes)
     })
-})
+    .catch(function(err) {
+        res.status(500).json(err)
+    })
+});
 
 
 app.listen(PORT, () => {
